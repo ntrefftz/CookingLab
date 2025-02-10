39747adadddeb68c08da63df6fde87f171a3c2d4
kwnggg <126821247+kwnggg@users.noreply.github.com>
2025-02-10 21:37:22 +0100

    app.mjs

---
+++ src/app.mjs
@@ -0,0 +1,24 @@
+import { notFound, estatico } from "./controladores.mjs";
+import { baseUrl } from './config.mjs';
+
+export function requestListener(req, res) {
+    let handler = null;
+
+    // procesa URLs que hacen referencia a ficheros / directorios que están dentro del directorio static
+    if (estatico(req, res)) {
+        return;
+    }
+
+    const url = new URL(`${baseUrl}${req.url}`);
+    const path = url.pathname;
+    switch(path) {
+        case "/saludos":
+            handler = saludo;
+            break;    
+        default:
+            handler = notFound;
+            break;
+    }
+    handler(req, res);
+
+};
