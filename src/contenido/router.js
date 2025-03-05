039f6d151cc10ba7b244fd9ad2cb210eddad1844
kwnggg <126821247+kwnggg@users.noreply.github.com>
2025-03-05 17:56:36 +0100

    Kai términos y contacto
    Base con términos y condiciones y contacto corregido

--- src/contenido/router.js
+++ src/contenido/router.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { viewContenidoAdmin, viewContenidoNormal, viewContacto } from './controllers.js';
+import { viewContenidoAdmin, viewContenidoNormal, viewContacto, viewCondiciones } from './controllers.js';
 
 const contenidoRouter = express.Router();
 
 contenidoRouter.get('/normal', viewContenidoNormal);
-
 contenidoRouter.get('/admin', viewContenidoAdmin);
-
 contenidoRouter.get('/contacto', viewContacto);
+contenidoRouter.get('/condiciones', viewCondiciones);
 
 export default contenidoRouter;
