89a5429481eaa7dedb0951b364efbb14d3c2ba0d
fernandochanglz <ferliu@ucm.es>
2025-03-06 17:09:55 +0100

    Recetas eliminar y editar

--- src/recetas/router.js
+++ src/recetas/router.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { viewRecetasDetalle, viewRecetasLista, viewModificarReceta } from './controllers.js';
+import { viewRecetasDetalle, viewRecetasLista, viewModificarReceta, eliminarReceta, modificarReceta } from './controllers.js';
 
 const recetasRouter = express.Router();
 
 recetasRouter.get('/catalogo', viewRecetasLista);
 recetasRouter.get('/receta', viewRecetasDetalle);
-recetasRouter.get('/modificar', viewModificarReceta);
+recetasRouter.get('/receta/modificar', viewModificarReceta);
+recetasRouter.get('/receta/eliminar', eliminarReceta);
+recetasRouter.post('/receta/modificar', modificarReceta);
 
 export default recetasRouter;
