cea7c93c78864fed7f358967d858d82cd410a0c4
kwnggg <126821247+kwnggg@users.noreply.github.com>
2025-03-06 18:21:28 +0100

    Kai ingredientes
    Ingredientes (editar, eliminar, ejs...)

--- src/recetas/controllers.js
+++ src/recetas/controllers.js
@@ -1,4 +1,7 @@
+import { Ingrediente } from './Ingredientes.js';
 import { Receta } from './Recetas.js';
+import { body } from 'express-validator';
+
 export function viewRecetasLista(req, res) {
     const rows = Receta.getAllRecetas();
     const contenido = 'paginas/catalogo';
@@ -10,7 +13,6 @@ export function viewRecetasLista(req, res) {
 }
 
 export function viewRecetasDetalle(req, res) {
-    // TODO
     const contenido = 'paginas/receta';
     const id = req.query.id;
     const receta = Receta.getRecetaById(id);
@@ -22,25 +24,107 @@ export function viewRecetasDetalle(req, res) {
 }
 
 export function viewModificarReceta(req, res) {
-    // TODO
-    const contenido = 'paginas/modificarReceta';
+    const contenido = 'paginas/editarReceta';
+    const id = req.query.id;
+    const receta = Receta.getRecetaById(id);
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        recetas: receta
+    });
+}
+
+export function eliminarReceta(req, res) {
+    const contenido = 'paginas/eliminada';
+    const id = req.query.id;
+    Receta.deleteReceta(id);
     res.render('pagina', {
         contenido,
         session: req.session
     });
 }
 
-/*
+export function modificarReceta(req, res) {
+    body('nombre').escape();
+    body('descripcion').escape();
+    body('difilcultad').escape();
+    body('tiempo_prep_segs').escape();
 
-export function administrarRecetas(req, res) {
-    // TODO
-    const contenido = 'paginas/administrarRecetas';
-    //if cocinero
+    const nombre = req.body.nombre.trim();
+    const descripcion = req.body.descripcion.trim();
+    const dificultad = req.body.dificultad.trim();
+    const tiempo_prep_segs = req.body.tiempo_prep_segs.trim();
+    const id = req.query.id;
+    const contenido = 'paginas/receta';
+    Receta.updateReceta(id, nombre, descripcion, tiempo_prep_segs*60, dificultad, 1);
+    const receta = Receta.getRecetaById(id);
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        recetas: receta
+    });
+}
 
-    //else if admin
 
+export function viewIngredientesLista(req, res) {
+    const rows = Ingrediente.getAllIngredientes();
+    const contenido = 'paginas/ingrediente';
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        ingredientes: rows
+    });
+}
+
+export function viewIngredientesDetalle(req, res) {
+    const contenido = 'paginas/ingredienteInd';
+    const id = req.query.id;
+    const ingrediente = Ingrediente.getIngredienteById(id);
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        ingredientes: ingrediente
+    });
+}
+
+export function viewModificarIngrediente(req, res) {
+    const contenido = 'paginas/editarIng';
+    const id = req.query.id;
+    const ingrediente = Ingrediente.getIngredienteById(id);
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        ingredientes: ingrediente
+    });
+}
+
+export function eliminarIngrediente(req, res) {
+    const contenido = 'paginas/eliminadaIng';
+    const id = req.query.id;
+    Ingrediente.deleteIngrediente(id);
     res.render('pagina', {
         contenido,
         session: req.session
     });
-}*/
+}
+
+export function modificarIngrediente(req, res) {
+    body('nombre').escape();
+    body('categoria').escape();
+    body('precio').escape();
+    body('stock').escape();
+
+    const nombre = req.body.nombre.trim();
+    const categoria = req.body.categoria.trim();
+    const precio = req.body.precio.trim();
+    const stock = req.body.stock.trim();
+    const id = req.query.id;
+    const contenido = 'paginas/ingredienteInd';
+    Ingrediente.updateIngrediente(id, nombre, categoria, precio, stock);
+    const ingrediente = Ingrediente.getIngredienteById(id);
+    res.render('pagina', {
+        contenido,
+        session: req.session,
+        ingredientes: ingrediente
+    });
+}
