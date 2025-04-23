77f060123e031d0e7784d9362844281a9c52458d
fernandochanglz <ferliu@ucm.es>
2025-04-23 12:05:02 +0200

    mostrar cesta vacia

--- src/modelos.js
+++ src/modelos.js
@@ -3,6 +3,10 @@ import { Ingrediente } from "./recetas/Ingredientes.js";
 import { Receta } from "./recetas/Recetas.js";
 import { Tiene } from "./recetas/Tiene.js"; // Nuevo import para Tiene
 import { CalendarioSemanal } from "./usuarios/CalendarioSemanal.js"; // Nuevo import para CalendarioSemanal
+import { Pedido } from "./pedidos/Pedidos.js"; // Nuevo import para Pedidos
+import { Realiza } from "./pedidos/Realiza.js"; // Nuevo import para Realiza
+import { Contiene } from "./pedidos/Contiene.js"; // Nuevo import para Contiene
+
 
 export function inicializaModelos(db) {
     Usuario.initStatements(db);
@@ -10,4 +14,7 @@ export function inicializaModelos(db) {
     Receta.initStatements(db); // Inicializa recetas
     Tiene.initStatements(db); // Inicializa las consultas de 'Tiene'
     CalendarioSemanal.initStatements(db); // Inicializa el calendario semanal
+    Pedido.initStatements(db); // Inicializa los pedidos
+    Realiza.initStatements(db); // Inicializa la relación entre usuarios y pedidos
+    Contiene.initStatements(db); // Inicializa la relación entre ingredientes y pedidos
 }
